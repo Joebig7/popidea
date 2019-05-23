cad32602f1c5ef67a2ffdee86661101ce59d81c7
JoeBig7 <1479762535@qq.com>
2019-05-23 18:08:57 +0800

    :bug: 增加分页

--- src/main/java/com/mamba/popidea/controller/IndustryController.java
+++ src/main/java/com/mamba/popidea/controller/IndustryController.java
@@ -2,11 +2,13 @@ package com.mamba.popidea.controller;
 
 import com.mamba.popidea.model.common.result.RestResp;
 import com.mamba.popidea.service.IndustryService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: JoeBig7
  * @date: 2019/5/23 16:55
  */
+@Api(value = "行业相关api",tags = "行业相关api")
 @RequestMapping("/industry")
 @RestController
 public class IndustryController {
@@ -24,7 +27,7 @@ public class IndustryController {
     @ApiOperation(value = "查询行业信息", notes = "查询行业信息")
     @GetMapping("/all")
     @ApiImplicitParam(paramType = "header", dataType = "string", name = "authorization")
-    public RestResp getAllIndustry() {
-        return new RestResp<>(industryService.getAllIndustryList());
+    public RestResp getAllIndustry(@RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo, @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+        return new RestResp<>(industryService.getAllIndustryList(pageNo,pageSize));
     }
 }
--- src/main/java/com/mamba/popidea/service/impl/IndustryServiceImpl.java
+++ src/main/java/com/mamba/popidea/service/impl/IndustryServiceImpl.java
@@ -1,9 +1,14 @@
 package com.mamba.popidea.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.mamba.popidea.dao.IndustryBeanMapper;
 import com.mamba.popidea.model.IndustryBean;
+import com.mamba.popidea.model.common.result.RestData;
 import com.mamba.popidea.service.IndustryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -12,13 +17,18 @@ import java.util.List;
  * @author: JoeBig7
  * @date: 2019/5/23 12:25
  */
+@Service
 public class IndustryServiceImpl implements IndustryService {
 
     @Autowired
     private IndustryBeanMapper industryBeanMapper;
 
     @Override
-    public List<IndustryBean> getAllIndustryList() {
-        return industryBeanMapper.findAllIndustryList();
+    public RestData<IndustryBean> getAllIndustryList(Integer pageNo, Integer pageSize) {
+        PageHelper.startPage(pageNo, pageSize);
+        List<IndustryBean> allIndustryList = industryBeanMapper.findAllIndustryList();
+        PageInfo<IndustryBean> pageInfo = new PageInfo<>(allIndustryList);
+        System.out.println(pageInfo.getPageNum()+"===="+pageInfo.getPageSize());
+        return new RestData<>(pageInfo.getList(), pageInfo.getTotal());
     }
 }
