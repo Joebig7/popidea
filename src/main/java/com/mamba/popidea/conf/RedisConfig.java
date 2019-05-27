75abf6a7653ed4a18b0d6daaabf82aab77126f3d
JoeBig7 <1479762535@qq.com>
2019-05-27 15:27:38 +0800

    :bug:

--- src/main/java/com/mamba/popidea/conf/RedisConfig.java
+++ src/main/java/com/mamba/popidea/conf/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.mamba.popidea.conf;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +30,6 @@ public class RedisConfig {
         template.setHashValueSerializer(RedisSerializer.json());
         template.afterPropertiesSet();
         return template;
-
     }
 
 
