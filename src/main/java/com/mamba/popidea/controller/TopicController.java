2579d74bf945c746128fa0e419d340545f5d56bd
JoeBig7 <1479762535@qq.com>
2019-05-30 18:16:52 +0800

    :bug: fix login expire time bug

--- src/main/java/com/mamba/popidea/controller/TopicController.java
+++ src/main/java/com/mamba/popidea/controller/TopicController.java
@@ -3,6 +3,7 @@ package com.mamba.popidea.controller;
 import com.mamba.popidea.model.common.result.RestResp;
 import com.mamba.popidea.service.TopicService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
+    @ApiImplicitParam(paramType = "header", dataType = "string", name = "Authorization", required = true)
     @ApiOperation(value = "获取话题列表", notes = "获取话题列表")
     @GetMapping("/get")
     public RestResp getTopicList(@ApiParam(required = false) @RequestParam(value = "key", required = false) String key, @RequestParam(value = "pageNo", defaultValue = "1") Integer pageNo, @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
