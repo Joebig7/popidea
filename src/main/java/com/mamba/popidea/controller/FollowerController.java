fbd3c7114693b5da49abf1fd398f3128104490da
JoeBig7 <1479762535@qq.com>
2019-06-10 12:29:55 +0800

    :sparkles:  add focus module

---
+++ src/main/java/com/mamba/popidea/controller/FollowerController.java
@@ -0,0 +1,18 @@
+package com.mamba.popidea.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @author: JoeBig7
+ * @date: 2019/6/10 12:28
+ */
+
+@Api(value = "关注相关api", tags = "关注")
+@RestController
+@RequestMapping("/follower")
+public class FollowerController {
+
+}
