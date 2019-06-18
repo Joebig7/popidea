abe333c7e5a355c6282497ea4e8200dbe33fb635
JoeBig7 <1479762535@qq.com>
2019-06-18 16:53:27 +0800

    :white_check_mark: login

--- src/test/java/com/mamba/popidea/user/UserTest.java
+++ src/test/java/com/mamba/popidea/user/UserTest.java
@@ -1,8 +1,16 @@
 package com.mamba.popidea.user;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @version 1.0
@@ -13,5 +21,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserTest {
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user/login")
+                .param("username", "zjf").param("password", "123")).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
 
 }
