b7dfff5f356356feafe52781de0e1ed327c381c5
JoeBig7 <1479762535@qq.com>
2019-08-06 16:27:36 +0800

    fix:update exception handler

--- src/main/java/com/mamba/popidea/handler/GlobalExceptionHandler.java
+++ src/main/java/com/mamba/popidea/handler/GlobalExceptionHandler.java
@@ -4,7 +4,8 @@ import com.google.common.collect.Maps;
 import com.mamba.popidea.exception.JsonException;
 import com.mamba.popidea.exception.RestException;
 import com.mamba.popidea.exception.ServiceException;
-import com.mamba.popidea.model.common.result.RestResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,8 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
-import static com.mamba.popidea.exception.ErrorCodes.PARAM_CHECK_ERROR;
-import static com.mamba.popidea.exception.ErrorCodes.WEB_PARAM_PARSE_ERROR;
+import static com.mamba.popidea.exception.ErrorCodes.*;
 
 /**
  * @version 1.0
@@ -26,33 +26,40 @@ import static com.mamba.popidea.exception.ErrorCodes.WEB_PARAM_PARSE_ERROR;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public RestResp exceptionHandler(Exception exception) {
+    public Map<String, Object> exceptionHandler(Exception exception) {
         if (exception instanceof JsonException) {
+            log.info(((JsonException) exception).getMsg());
             JsonException jsonException = JsonException.newInstance();
             return getErrorMap(jsonException.getMsg(), jsonException.getCode().getErrorCode());
         } else if (exception instanceof RestException) {
+            log.info(((RestException) exception).getMsg());
             return getErrorMap(((RestException) exception).getCode().getErrorMessage(), ((RestException) exception).getCode().getErrorCode());
         } else if (exception instanceof ServiceException) {
+            log.info(((ServiceException) exception).getMsg());
             return getErrorMap(((ServiceException) exception).getCode().getErrorMessage(), ((ServiceException) exception).getCode().getErrorCode());
         } else if (exception instanceof HttpMessageNotReadableException) {
+            log.info(exception.getMessage());
             return getErrorMap(WEB_PARAM_PARSE_ERROR.getErrorMessage(), WEB_PARAM_PARSE_ERROR.getErrorCode());
         } else if (exception instanceof BindException) {
+            log.info(exception.getMessage());
             StringBuilder errorMsg = new StringBuilder();
             ((BindException) exception).getAllErrors().forEach(msg -> errorMsg.append(msg));
             return getErrorMap(errorMsg.toString(), PARAM_CHECK_ERROR.getErrorCode());
+        } else {
+            log.info(exception.getMessage());
+            return getErrorMap(SERVICE_ERROR.getErrorMessage(), SERVICE_ERROR.getErrorCode());
         }
-
-        return null;
     }
 
-    public static RestResp getErrorMap(String msg, Integer errorCode) {
+    public static Map<String, Object> getErrorMap(String msg, Integer errorCode) {
         Map<String, Object> errorMap = Maps.newHashMap();
         errorMap.put("msg", msg);
         errorMap.put("errorCode", errorCode);
-        return new RestResp<>(errorMap);
-
+        return errorMap;
     }
 
 }
